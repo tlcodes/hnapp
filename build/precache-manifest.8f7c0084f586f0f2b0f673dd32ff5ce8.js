24672133bd472302efe653f4c1549acf8578e3ac
Thomas Leroy <imtrip@gmail.com>
2018-10-11 16:38:12 +0200

    add build

---
+++ build/precache-manifest.8f7c0084f586f0f2b0f673dd32ff5ce8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6a3d72d015ab8b448be",
+    "url": "/hnapp/build/static/js/runtime~main.b6a3d72d.js"
+  },
+  {
+    "revision": "65f51d5f1e47f1ee7b5c",
+    "url": "/hnapp/build/static/js/main.65f51d5f.chunk.js"
+  },
+  {
+    "revision": "a8c7d3ee119674051da5",
+    "url": "/hnapp/build/static/js/1.a8c7d3ee.chunk.js"
+  },
+  {
+    "revision": "65f51d5f1e47f1ee7b5c",
+    "url": "/hnapp/build/static/css/main.77c78566.chunk.css"
+  },
+  {
+    "revision": "0be76f7a7bc3b628958dcd235f032eea",
+    "url": "/hnapp/build/index.html"
+  }
+];
